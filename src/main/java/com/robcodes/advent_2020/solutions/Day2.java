851fe8cdcc4c7c9d89744b1d065d395b93393e98
Rob <Robertcarterusc@gmail.com>
2020-12-02 09:33:42 -0500

    Add Part 2 from Day 2

--- src/main/java/com/robcodes/advent_2020/solutions/Day2.java
+++ src/main/java/com/robcodes/advent_2020/solutions/Day2.java
@@ -12,26 +12,37 @@ public class Day2 {
 
     public static void main(String[] args) throws IOException {
         try (InputStreamReader streamReader = new InputStreamReader(
-                Objects.requireNonNull(Day2.class.getClassLoader().getResourceAsStream("input//day2.txt")),
+                Objects.requireNonNull(Day2.class.getClassLoader().getResourceAsStream("input//day2-pt1.txt")),
                 StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(streamReader)) {
             String line;
-            int counter = 0;
+            int part1Counter = 0;
+            int part2Counter = 0;
             while ((line = reader.readLine()) != null) {
                 String[] passwordAndPasswordRules = line.split(":");
                 String[] passwordRules = passwordAndPasswordRules[0].split(" ");
                 String[] range = passwordRules[0].split("-");
                 String regex = passwordRules[1];
                 int i = 0;
+                //Part 1
                 Matcher matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(passwordAndPasswordRules[1]);
                 while (matcher.find()) {
                     i++;
                 }
-                if (i >= Long.parseLong(range[0]) && i <= Long.parseLong(range[1])) {
-                    counter++;
+                int range1 = Integer.parseInt(range[0]);
+                int range2 = Integer.parseInt(range[1]);
+                if (i >= range1 && i <= range2) {
+                    part1Counter++;
+                }
+                //Part 2
+                char c1 = passwordAndPasswordRules[1].charAt(range1);
+                char c2 = passwordAndPasswordRules[1].charAt(range2);
+                if (c1 == regex.charAt(0) ^ c2 == regex.charAt(0)) {
+                    part2Counter++;
                 }
             }
-            System.out.println(counter);
+            System.out.println("Day 2 Part 1 Answer " + part1Counter);
+            System.out.println("Day 2 Part 2 Answer " + part2Counter);
         }
     }
 }
